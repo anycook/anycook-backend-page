15af9159977f8616fc9cabfc7db5657cb3ad69bb
Jan Graßegger <jan@anycook.de>
2014-11-21 02:30:50 +0100

    added ability to update and delete mailproviders

--- app/scripts/views/MailProviderView.js
+++ app/scripts/views/MailProviderView.js
@@ -8,12 +8,48 @@ define([
     return Backbone.View.extend({
         tagName : 'tr',
         className : 'mailProvider',
+        events      : {
+            'click .change'   : 'enableChangeMode',
+            'click .delete'   : 'delete',
+            'click .save'     : 'save',
+            'click .cancel'   : 'disableChangeMode'
+        },
         initialize : function() {
             this.render();
         },
         render : function() {
             var data = this.model.toJSON();
             this.$el.html(mailProviderViewTemplate(data));
+        },
+        enableChangeMode : function() {
+            this.$('.shortName').html('<input type="text" class="form-control input-sm" value="' + this.model.get('shortName') + '">');
+            this.$('.fullName').html('<input type="text" class="form-control input-sm" value="' + this.model.get('fullName') + '">');
+            this.$('.redirect').html('<input type="text" class="form-control input-sm" value="' + this.model.get('redirect') + '">');
+            this.$('.change-delete').hide();
+            this.$('.save-cancel').show();
+        },
+        disableChangeMode : function() {
+            this.$('.shortName').text(this.model.get('shortName'));
+            this.$('.fullName').text(this.model.get('fullName'));
+            var redirect = this.model.get('redirect');
+            this.$('.redirect').html('<a href="'+redirect+'">'+redirect+'</a>');
+            this.$('.change-delete').show();
+            this.$('.save-cancel').hide();
+        },
+        save : function() {
+            var shortName = this.$('.shortName input').val();
+            var fullName = this.$('.fullName input').val();
+            var redirect = this.$('.redirect input').val();
+            this.model.set('shortName', shortName);
+            this.model.set('fullName', fullName);
+            this.model.set('redirect', redirect);
+            this.model.save();
+            this.disableChangeMode();
+        },
+        delete : function() {
+            this.model.collection.remove(this.model);
+            this.model.destroy();
+            this.$el.remove();
         }
     });
 });
